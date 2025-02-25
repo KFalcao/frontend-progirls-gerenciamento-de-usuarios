25356848eda09442815e9eaebdb6b35488b520e1
KFalcao <karollinefalcao@gmail.com>
2025-02-24 22:45:21 -0300

    :boom: fix:correção JSON/conversão js->jsx

---
+++ src/pages/ForgotPassword.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ForgotPassword() {
+  return (
+    <main className="bg-gradient-to-l from-green-800 via-slate-50 to-pink-500 w-full h-full">
+      <button>Voltar</button>
+      <img src="" alt="" />
+    </main>
+  );
+}
+
+export default ForgotPassword;
